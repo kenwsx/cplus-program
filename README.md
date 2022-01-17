117440fe7a827ab7bc9e352e342b3c87a4ca1090
Ken_Chang <ken205115331@gmail.com>
2022-01-18 00:09:49 +0800

    Hello World!

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # cplus-program
+
+測試程式，定期上傳
--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 int main(){
 
+        std::cout << "Hello World" << std::endl;
 
-  printf("Hello world");
-  return 0;
+        return 0;
 }
