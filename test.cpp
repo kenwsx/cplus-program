64947afb7e0213d285ac041903a4a17e26affa22
Ken_Chang <ken205115331@gmail.com>
2022-03-24 00:19:41 +0800

    vs code test

--- test.cpp
+++ test.cpp
@@ -1,39 +1,10 @@
 #include <iostream>
 #include<cmath>
 
-class Point
-{
-public:
-	float x, y;
-  Point &operator=(const Point& buf)
-  {
-    this->x = buf.x;
-    this->y = buf.y;
 
-    std::cout << "overload" << '\n';
-    return *this;
-  }
-};
-
-class Segment
-{
-public:
-	Segment()
-	{
-	}
-	Segment(Point _a, Point _b)
-	{
-		a = _a;
-		b = _b;
-	}
-
-private:
-	Point a, b;
-};
 int main()
 {
-    double c = (double)1/2 * abs(-1*1.5);
-    std::cout << c << '\n';
+    std::cout << "Hello";
 
 
     return 0;
